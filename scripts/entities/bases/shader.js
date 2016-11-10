bbc3d833ec8d6d90537843dd9c8ed67821117979
Leon <lowpolygonum@gmail.com>
2016-11-10 01:46:36 +0100

    clean

--- scripts/entities/bases/shader.js
+++
@@ -1,7 +0,0 @@
-
-function Shader (vert, frag, uniforms)
-{
-	this.program = twgl.createProgramFromSources(gl, [vert, frag]);
-	this.info = twgl.createProgramInfoFromProgram(gl, this.program);
-	this.uniforms = uniforms || {};
-}
